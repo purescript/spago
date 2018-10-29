917a2a1504179ab4b04a4b2f7c119e2fe2de03f9
justinwoo <moomoowoo@gmail.com>
2018-10-29 23:14:14 +0200

    it's not 2010 anymore, we can use 'say'

--- script/prepare-tar.pl
+++ script/prepare-tar.pl
@@ -2,11 +2,10 @@
 
 use warnings;
 use strict;
+use feature 'say';
 
-print "\n";
-print "running stack install";
-print "\n";
-print `stack install`;
+say "running stack install";
+say `stack install`;
 
 my $stack_local_install_root = `stack path --local-install-root`;
 chomp $stack_local_install_root;
@@ -15,14 +14,10 @@ my $bin_path = "$stack_local_install_root/bin/spacchetti";
 
 my $target_path = "spacchetti";
 
-print "\n";
-print "copying built bin: $bin_path -> $target_path";
-print "\n";
-print `cp $bin_path $target_path`;
+say "copying built bin: $bin_path -> $target_path";
+say `cp $bin_path $target_path`;
 
 my $zip_path = "$ENV{TRAVIS_OS_NAME}.tar.gz";
 
-print "\n";
-print "zipping to $zip_path";
-print "\n";
-print `tar -zcvf $zip_path $target_path`;
+say "zipping to $zip_path";
+say `tar -zcvf $zip_path $target_path`;
