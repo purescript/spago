b56dfd0463961b8150a93bc3e1200fc06ca3bf87
Fabrizio Ferrai <fabrizio.ferrai@gmail.com>
2018-12-13 00:52:12 +0200

    Fix #31: add tests for most of spago commands

---
+++ test/spago-test.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+
+import os
+from utils import expect_success, expect_failure, fail, run_for, check_fixture
+
+
+## spago init
+
+expect_success(
+    ['spago', 'init'],
+    "Spago should have set up a project"
+)
+
+expect_failure(
+    ['spago', 'init'],
+    "Spago should refuse to overwrite an existing project without -f"
+)
+
+expect_success(
+    ['spago', 'init', '-f'],
+    "Spago should always succeed in doing init with force"
+)
+
+
+## spago install
+
+# Run `install` once and kill it soon to simulate failure
+run_for(0.5, ['spago', 'install', '-j', '3'])
+
+expect_success(
+    ['spago', 'install'],
+    "Subsequent installs should succeed anyways"
+)
+
+
+## spago build
+
+expect_success(
+    ['spago', 'build'],
+    "Spago should build successfully"
+)
+
+
+## spago test
+
+expect_success(
+    ['spago', 'test'],
+    "Spago should test successfully"
+)
+
+
+## spago bundle
+
+expect_success(
+    ['spago', 'bundle', '--to', 'bundle.js'],
+    "Spago should bundle successfully"
+)
+
+check_fixture('bundle.js')
+
+
+## spago make-module
+
+expect_success(
+    ['spago', 'make-module', '--to', 'module.js'],
+    "Spago should successfully make a module"
+)
+
+check_fixture('module.js')
+
+
+## Cleanup after tests
+
+expect_success(
+    ['rm', '-rf', '.spago', 'src', 'test', 'packages.dhall', 'spago.dhall', 'bundle.js', 'module.js', 'output'],
+    "Cleanup should empty the project folder"
+)
