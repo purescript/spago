51b499efb8946b553e83b5a8d03c170c7cf25567
Fabrizio Ferrai <fabrizio.ferrai@gmail.com>
2019-02-03 21:52:14 +0200

    Fix #88: implement handling of local packages in the package-set

--- app/Spago/Spacchetti.hs
+++ app/Spago/Spacchetti.hs
@@ -3,8 +3,10 @@ module Spago.Spacchetti where
 import           Data.Aeson
 import           Data.Map     (Map)
 import           Data.Text    (Text)
+import qualified Data.Text    as Text
 import qualified Dhall
 import           GHC.Generics (Generic)
+import           Network.URI  (parseURI)
 
 
 -- | Matches the packages definition of Spacchetti Package.dhall/psc-package
@@ -15,7 +17,7 @@ newtype PackageName = PackageName { packageName :: Text }
 -- | A spacchetti package.
 data Package = Package
   { dependencies :: [PackageName] -- ^ list of dependency package names
-  , repo         :: Text          -- ^ the git repository
+  , repo         :: Repo          -- ^ the git repository
   , version      :: Text          -- ^ version string (also functions as a git ref)
   }
   deriving (Show, Generic)
@@ -25,3 +27,17 @@ instance FromJSON Package
 
 type Packages = Map PackageName Package
 
+data Repo
+  = Local Text
+  | Remote Text
+  deriving (Show, Generic)
+
+instance ToJSON Repo
+instance FromJSON Repo
+
+instance Dhall.Interpret Repo where
+  autoWith _ = makeRepo <$> Dhall.strictText
+    where
+      makeRepo path = case parseURI $ Text.unpack path of
+        Just _uri -> Remote path
+        Nothing   -> Local path
