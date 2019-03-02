1a52fe2fd9e238361a32528d5aa6d4e3f5c5b5bf
Fabrizio Ferrai <fabrizio.ferrai@gmail.com>
2019-03-02 20:59:04 +0200

    Install windows binary with NPM (#121)
    This also bumps the version to 0.7.0.0

---
+++ npm/install.js
@@ -0,0 +1,21 @@
+"use strict";
+
+// The following implementation comes from
+// https://github.com/justinwoo/npm-psc-package-bin-simple/blob/4d4efa6a4e2008c8a0a71f0b189c14c31b88e47b/install.js
+
+const https = require("follow-redirects").https;
+const tar = require("tar");
+const shell = require("shelljs");
+const version = "PACKAGE_VERSION";
+const platform = { win32: "windows", darwin: "osx" }[process.platform] || "linux";
+
+https.get(
+    `https://github.com/spacchetti/spago/releases/download/${version}/${platform}.tar.gz`,
+    res => res.pipe(
+        tar.x({"C": './'}).on("finish", () => {
+            if (shell.test("-f", "./spago")) {
+                shell.mv("./spago", "./spago.exe")
+            }
+        })
+    )
+);
