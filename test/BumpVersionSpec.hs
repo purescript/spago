5e26ed1fe17778d87ee198e16d61d8d2677d8b54
Gareth Daniel Smith <garethdanielsmith@gmail.com>
2019-06-22 09:16:52 +0100

    Move the bump-version tests to their own module, and make them all independent.

---
+++ test/BumpVersionSpec.hs
@@ -0,0 +1,61 @@
+module BumpVersionSpec (spec) where
+
+import           Prelude        hiding (FilePath)
+import qualified System.IO.Temp as Temp
+import           Test.Hspec     (Spec, around_, before_, describe, it, shouldBe)
+import           Turtle         (Text, cptree, decodeString)
+import           Utils          (getHighestTag, git, shouldBeFailure,
+                                 shouldBeSuccess, spago, withCwd)
+
+
+setup :: IO () -> IO ()
+setup cmd = do
+  Temp.withTempDirectory "test/" "bump-version-test" $ \temp -> do
+    let tempPath = decodeString temp
+    cptree "test/bump-version-test" tempPath
+    withCwd tempPath cmd
+
+initGit :: IO ()
+initGit = do
+  git ["init"] >>= shouldBeSuccess
+  git ["add", "--all"] >>= shouldBeSuccess
+  git ["commit", "--message", "Initial commit"] >>= shouldBeSuccess
+
+initGitTag :: Text -> IO ()
+initGitTag tag = do
+  initGit
+  git ["tag", "--annotate", tag, "--message", ""] >>= shouldBeSuccess
+
+spec :: Spec
+spec = around_ setup $ do
+
+  describe "spago bump-version" $ do
+
+    it "Spago should complain when no git repo exists" $ do
+
+      spago ["bump-version", "minor"] >>= shouldBeFailure
+
+    before_ initGit $ it "Spago should use v0.0.0 as initial version" $ do
+
+      spago ["bump-version", "patch"] >>= shouldBeSuccess
+      getHighestTag >>= (`shouldBe` Just "v0.0.1")
+
+    before_ (initGitTag "v1.3.4") $ it "Spago should bump patch version" $ do
+
+      spago ["bump-version", "patch"] >>= shouldBeSuccess
+      getHighestTag >>= (`shouldBe` Just "v1.3.5")
+
+    before_ (initGitTag "v1.3.4") $ it "Spago should bump minor version" $ do
+
+      spago ["bump-version", "minor"] >>= shouldBeSuccess
+      getHighestTag >>= (`shouldBe` Just "v1.4.0")
+
+    before_ (initGitTag "v1.3.4") $ it "Spago should bump major version" $ do
+
+      spago ["bump-version", "major"] >>= shouldBeSuccess
+      getHighestTag >>= (`shouldBe` Just "v2.0.0")
+
+    before_ initGit $ it "Spago should set exact version" $ do
+
+      spago ["bump-version", "v3.1.5"] >>= shouldBeSuccess
+      getHighestTag >>= (`shouldBe` Just "v3.1.5")
---
+++ test/bump-version-test/spago.dhall
@@ -0,0 +1,11 @@
+{-
+Welcome to a Spago project!
+You can edit this file as you like.
+-}
+{ name =
+    "bump-version-test"
+, dependencies =
+    [ "effect", "console", "psci-support" ]
+, packages =
+    ./packages.dhall
+}
