727f8a12d5cf7cd0c13e06a9c76d92126e1ee6f2
Fabrizio Ferrai <fabrizio.ferrai@gmail.com>
2019-07-26 16:32:11 +0300

    Fix wording

--- src/Spago/Version.hs
+++ src/Spago/Version.hs
@@ -16,6 +16,7 @@ import qualified Safe.Foldable as Safe
 import qualified Spago.Bower   as Bower
 import           Spago.DryRun     (DryRun(..), DryAction(..), runDryActions)
 import qualified Spago.Git     as Git
+import Spago.Messages (surroundQuote)
 
 
 data VersionBump
@@ -96,5 +97,7 @@ bumpVersion dryRun spec = do
     die $ "A new " <> Bower.bowerPath <> " has been generated. Please commit this and run `bump-version` again."
 
   runDryActions dryRun
-    $ DryAction ("create new version tag " <> unparseVersion newVersion) (tagNewVersion oldVersion newVersion)
+    $ DryAction
+        ("create new git tag " <> surroundQuote (unparseVersion newVersion))
+        (tagNewVersion oldVersion newVersion)
     :| []
