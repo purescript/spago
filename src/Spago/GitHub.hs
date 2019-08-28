e988bbab99dd3c471c40a373b7d2e39bc5cc0e21
Fabrizio Ferrai <fabrizio.ferrai@gmail.com>
2019-08-28 09:55:16 +0200

    Add 'spago login' command (#391)
    This adds the `spago login` command, which works much like
    `pulp login` does, and allows the user to add a GitHub OAuth
    token to Spago's cache.
    
    This is necessary in order for the Pursuit's docs upload to work

---
+++ src/Spago/GitHub.hs
@@ -0,0 +1,85 @@
+{-# LANGUAGE ViewPatterns #-}
+module Spago.GitHub where
+
+import           Spago.Prelude
+
+import qualified Control.Retry      as Retry
+import qualified Data.Text          as Text
+import qualified Data.Text.Encoding
+import qualified GitHub
+import qualified System.Environment
+
+import qualified Spago.GlobalCache  as GlobalCache
+import qualified Spago.Messages     as Messages
+
+
+tagCacheFile, tokenCacheFile :: IsString t => t
+tagCacheFile = "package-sets-tag.txt"
+tokenCacheFile = "github-token.txt"
+
+
+login :: Spago m => m ()
+login = do
+ maybeToken <- liftIO (System.Environment.lookupEnv githubTokenEnvVar)
+ globalCacheDir <- GlobalCache.getGlobalCacheDir
+
+ case maybeToken of
+   Nothing -> die Messages.getNewGitHubToken
+   Just (Text.pack -> token) -> do
+     echo "Token read, authenticating with GitHub.."
+     username <- getUsername token
+     echo $ "Successfully authenticated as " <> surroundQuote username
+     writeTextFile (Text.pack $ globalCacheDir </> tokenCacheFile) token
+  where
+    getUsername token = do
+      result <- liftIO $ GitHub.executeRequest
+        (GitHub.OAuth $ Data.Text.Encoding.encodeUtf8 token)
+        GitHub.userInfoCurrentR
+      case result of
+        Left err              -> die $ Messages.failedToReachGitHub err
+        Right GitHub.User{..} -> pure $ GitHub.untagName userLogin
+
+
+readToken :: Spago m => m Text
+readToken = readFromEnv <|> readFromFile
+  where
+    readFromEnv = liftIO (System.Environment.lookupEnv githubTokenEnvVar) >>= \case
+      Nothing -> empty
+      Just (Text.pack -> token) -> return token
+
+    readFromFile = do
+      globalCacheDir <- GlobalCache.getGlobalCacheDir
+      assertDirectory globalCacheDir
+      readTextFile $ pathFromText $ Text.pack $ globalCacheDir </> tokenCacheFile
+
+
+getLatestPackageSetsTag :: Spago m => m (Either SomeException Text)
+getLatestPackageSetsTag = do
+  globalCacheDir <- GlobalCache.getGlobalCacheDir
+  assertDirectory globalCacheDir
+  let globalPathToCachedTag = globalCacheDir </> tagCacheFile
+  let writeTagCache releaseTagName = writeTextFile (Text.pack globalPathToCachedTag) releaseTagName
+  let readTagCache = try $ readTextFile $ pathFromText $ Text.pack globalPathToCachedTag
+  let downloadTagToCache =
+        try (Retry.recoverAll (Retry.fullJitterBackoff 50000 <> Retry.limitRetries 5) $ \_ -> getLatestRelease) >>= \case
+          Left (err :: SomeException) -> echoDebug $ Messages.failedToReachGitHub err
+          Right releaseTagName -> writeTagCache releaseTagName
+
+  whenM (shouldRefreshFile globalPathToCachedTag) downloadTagToCache
+
+  readTagCache
+
+  where
+    getLatestRelease :: Spago m => m Text
+    getLatestRelease = do
+      maybeToken :: Either SomeException Text <- try readToken
+      f <- case hush maybeToken of
+        Nothing -> pure GitHub.executeRequest'
+        Just token -> do
+          echoDebug "Using cached GitHub token for getting the latest release.."
+          pure $ GitHub.executeRequest (GitHub.OAuth $ Data.Text.Encoding.encodeUtf8 token)
+      result <- liftIO $ f $ GitHub.latestReleaseR "purescript" "package-sets"
+
+      case result of
+        Left err                 -> die $ Messages.failedToReachGitHub err
+        Right GitHub.Release{..} -> pure releaseTagName
