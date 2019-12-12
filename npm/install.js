85808c237803d10e7248470943e23bbe54a3eba1
Paul Burt <41005722+beebpeeb@users.noreply.github.com>
2019-12-12 08:00:41 +0000

    Replaced follow-redirects NPM dependency (#522)
    Replaced `follow-redirects` module with `request`.
    The latter follows redirects by default and also respects
    HTTP(S) proxy environment variables, allowing spago to be
    installed behind a proxy.
    
    Fixes #460

--- npm/install.js
+++ npm/install.js
@@ -3,12 +3,10 @@
 // The following implementation comes from
 // https://github.com/justinwoo/npm-psc-package-bin-simple/blob/4d4efa6a4e2008c8a0a71f0b189c14c31b88e47b/install.js
 
-const https = require("follow-redirects").https;
+const request = require("request");
 const tar = require("tar");
 const version = "PACKAGE_VERSION";
 const platform = { win32: "windows", darwin: "osx" }[process.platform] || "linux";
+const url = `https://github.com/spacchetti/spago/releases/download/${version}/${platform}.tar.gz`
 
-https.get(
-    `https://github.com/spacchetti/spago/releases/download/${version}/${platform}.tar.gz`,
-    res => res.pipe(tar.x({"C": './'}))
-);
+request.get(url).pipe(tar.x({"C": './'}));
