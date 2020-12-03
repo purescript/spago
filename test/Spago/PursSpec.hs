ffdc4229ce3dd1fa0ec60577d84527938d25a597
Fabrizio Ferrai <fabrizio.ferrai@gmail.com>
2020-12-03 17:25:25 +0200

    Fix tests

--- test/Spago/PursSpec.hs
+++ test/Spago/PursSpec.hs
@@ -1,32 +1,32 @@
-module Spago.Command.PathSpec (spec) where
+module Spago.PursSpec (spec) where
 
 import            Prelude
 import            Test.Hspec
 
 import            Spago.Prelude hiding (link)
 import            Spago.Env
-import qualified  Spago.Command.Path   as Path
+import qualified  Spago.Purs as Purs
 
 spec :: Spec
 spec = do
-  describe "Path findFlag" $ do
+  describe "findFlag" $ do
     it "[\"-o\", \"something\"]" $ do
-      let a = fromMaybe "" $ Path.findFlag 'o' "output" [PursArg "-o" , PursArg "something"]
+      let a = fromMaybe "" $ Purs.findFlag 'o' "output" [PursArg "-o" , PursArg "something"]
       let b = "something"
       a `shouldBe` b
     it "[\"--output\", \"something\"]" $ do
-      let a = fromMaybe "" $ Path.findFlag 'o' "output" [PursArg "--output" , PursArg "something"]
+      let a = fromMaybe "" $ Purs.findFlag 'o' "output" [PursArg "--output" , PursArg "something"]
       let b = "something"
       a `shouldBe` b
     it "[\"-o something\"]" $ do
-      let a = fromMaybe "" $ Path.findFlag 'o' "output" [PursArg "-o something"]
+      let a = fromMaybe "" $ Purs.findFlag 'o' "output" [PursArg "-o something"]
       let b = "something"
       a `shouldBe` b
     it "[\"--output something\"]" $ do
-      let a = fromMaybe "" $ Path.findFlag 'o' "output" [PursArg "--output something"]
+      let a = fromMaybe "" $ Purs.findFlag 'o' "output" [PursArg "--output something"]
       let b = "something"
       a `shouldBe` b
     it "[\"--output=something\"]" $ do
-      let a = fromMaybe "" $ Path.findFlag 'o' "output" [PursArg "--output=something"]
+      let a = fromMaybe "" $ Purs.findFlag 'o' "output" [PursArg "--output=something"]
       let b = "something"
       a `shouldBe` b
