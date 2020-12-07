d57a600b0ea46ad6a170e29e06e4bc79c2468cf3
Fabrizio Ferrai <fabrizio.ferrai@gmail.com>
2020-12-07 16:35:59 +0200

    Fix artifact names in release CI

--- npm/install.js
+++ npm/install.js
@@ -1,12 +1,9 @@
 "use strict";
 
-// The following implementation comes from
-// https://github.com/justinwoo/npm-psc-package-bin-simple/blob/4d4efa6a4e2008c8a0a71f0b189c14c31b88e47b/install.js
-
 const request = require("request");
 const tar = require("tar");
 const version = "PACKAGE_VERSION";
 const platform = { win32: "windows", darwin: "macOS" }[process.platform] || "linux";
-const url = `https://github.com/purescript/spago/releases/download/${version}/${platform}.tar.gz`
+const url = `https://github.com/purescript/spago/releases/download/${version}/${platform}-latest.tar.gz`
 
 request.get(url).pipe(tar.x({"C": './'}));
