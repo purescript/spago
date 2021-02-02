eff5611237471fd72a9f7f35bf75fd5491cc1d03
Colin Wahl <colin.t.wahl@gmail.com>
2021-02-01 23:03:32 -0800

    add test cases for failing the build on direct import from transitive dependency and warning on unused package dependencies

--- test/SpagoSpec.hs
+++ test/SpagoSpec.hs
@@ -416,6 +416,23 @@ spec = around_ setup $ do
       cd ".."
       testdir "output" `shouldReturn` False
 
+    describe "import checking" $ do
+      it "Spago should fail on direct project imports from transitive dependencies" $ do
+        spago ["init"] >>= shouldBeSuccess
+        rm "spago.dhall"
+        writeTextFile "spago.dhall" $ "{ name = \"check-imports\", dependencies = [\"effect\"], packages = ../packages.dhall }"
+        rm "src/Main.purs"
+        writeTextFile "src/Main.purs" "module Main where\nimport Prelude\nmain = unit"
+        spago ["build"] >>= shouldBeFailureStderr "check-direct-import-transitive-dependency.txt"
+
+     it "Spago should warn on unused dependencies" $ do
+        spago ["init"] >>= shouldBeSuccess
+        rm "spago.dhall"
+        writeTextFile "spago.dhall" $ "{ name = \"check-imports\", dependencies = [\"effect\", \"prelude\"], packages = ../packages.dhall }"
+        rm "src/Main.purs"
+        writeTextFile "src/Main.purs" "module Main where\nimport Prelude\nmain = unit"
+        spago ["build"] >>= shouldBeFailureStderr "check-unused-dependency.txt"
+
     describe "alternate backend" $ do
 
       it "Spago should use alternate backend if option is specified" $ do
