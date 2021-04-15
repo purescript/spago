cb39dd2e5b6ca4160e9d0cce7cb8551141cee5de
Vladimir Kalnitsky <klntsky@gmail.com>
2021-04-15 22:24:55 +0300

    Bump version to v0.0.11 (#47)

--- docs-search/src/Docs/Search/Config.purs
+++ docs-search/src/Docs/Search/Config.purs
@@ -7,7 +7,7 @@ import Prelude
 import Data.Newtype (wrap)
 
 version :: String
-version = "0.0.10"
+version = "0.0.11"
 
 mkShapeScriptPath :: String -> String
 mkShapeScriptPath shape = "./index/types/" <> shape <> ".js"
