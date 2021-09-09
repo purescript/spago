28f1cfd886ad1087b785fbc8ae816247af54da8a
Jordan Martinez <jordanalex.martinez@protonmail.com>
2021-09-09 08:21:24 -0700

    Add test: Let at WithinField

--- test/SpagoSpec.hs
+++ test/SpagoSpec.hs
@@ -449,6 +449,12 @@ spec = around_ setup $ do
           "alternative4-root-field-with-right-nested-before.dhall"
           "alternative4-root-field-with-right-nested-success.dhall"
 
+      it "... alternative4-root-recordlit-let.dhall" $ do
+
+        checkInstallFixtureSucceed
+          "alternative4-root-recordlit-let-before.dhall"
+          "alternative4-root-recordlit-let-success.dhall"
+
     it "Spago should fail when the alternate config file doesn't exist" $ do
       spago ["init"] >>= shouldBeSuccess
       spago ["install", "-x", "test.dhall"] >>= shouldBeFailureStderr "alternate-config-missing.txt"
---
+++ test/fixtures/alternative4-root-recordlit-let-success.dhall
@@ -0,0 +1,7 @@
+{ name = "my-project"
+, dependencies =
+      (let x = [ "console", "effect", "prelude", "psci-support" ] in x)
+    # [ "newtype" ]
+, packages = ./packages.dhall
+, sources = [ "src/**/*.purs", "test/**/*.purs" ]
+}
