eb3a92e6f266a217fd002210bd8669bbaa7a98ca
Fabrizio Ferrai <fabrizio.ferrai@gmail.com>
2023-05-04 13:32:04 +0300

    Use purs.cmd on Windows

--- spaghetto/src/Spago/Purs.purs
+++ spaghetto/src/Spago/Purs.purs
@@ -8,6 +8,8 @@ import Data.Codec.Argonaut.Record as CAR
 import Data.Profunctor as Profunctor
 import Data.Set as Set
 import Data.String as String
+import Node.Platform as Platform
+import Node.Process as Process
 import Registry.Internal.Codec as Internal.Codec
 import Registry.Version as Version
 import Spago.Cmd as Cmd
@@ -25,22 +27,41 @@ type Purs =
 
 getPurs :: forall a. Spago (LogEnv a) Purs
 getPurs =
-  Cmd.exec "purs" [ "--version" ] Cmd.defaultExecOptions { pipeStdout = false, pipeStderr = false } >>= case _ of
-    Left err -> do
-      logDebug $ show err
-      die [ "Failed to find purs. Have you installed it, and is it in your PATH?" ]
-    -- Drop the stuff after a space: dev builds look like this: 0.15.6 [development build; commit: 8da7e96005f717f03d6eee3c12b1f1416659a919]
-    -- Drop the stuff after a hyphen: prerelease builds look like this: 0.15.6-2
-    Right r -> case parseLenientVersion (dropStuff "-" $ dropStuff " " r.stdout) of
-      Left _err -> die $ "Failed to parse purs version. Was: " <> r.stdout
-      -- Fail if Purs is lower than 0.15.4
-      Right v ->
-        if Version.minor v >= 15 && Version.patch v >= 4 then
-          pure { cmd: "purs", version: v }
-        else
-          die [ "Unsupported PureScript version " <> Version.print v, "Please install PureScript v0.15.4 or higher." ]
+  case Process.platform of
+    Just Platform.Win32 -> do
+      -- On Windows, we often need to call `purs.cmd` instead of `purs`
+      pursVersion "purs.cmd" >>= case _ of
+        Right r -> parseVersionOutput "purs.cmd" r
+        Left err' -> do
+          logDebug [ "Failed to find purs.cmd. Trying with just purs...", show err' ]
+          pursVersion "purs" >>= case _ of
+            Right r -> parseVersionOutput "purs" r
+            Left err -> complain err
+    _ -> do
+      -- On other platforms, we just call `purs`
+      pursVersion "purs" >>= case _ of
+        Right r -> parseVersionOutput "purs" r
+        Left err -> complain err
   where
+  pursVersion cmd = Cmd.exec cmd [ "--version" ] Cmd.defaultExecOptions { pipeStdout = false, pipeStderr = false }
+
   dropStuff pattern = fromMaybe "" <<< Array.head <<< String.split (String.Pattern pattern)
+
+  complain err = do
+    logDebug $ show err
+    die [ "Failed to find purs. Have you installed it, and is it in your PATH?" ]
+
+  -- Drop the stuff after a space: dev builds look like this: 0.15.6 [development build; commit: 8da7e96005f717f03d6eee3c12b1f1416659a919]
+  -- Drop the stuff after a hyphen: prerelease builds look like this: 0.15.6-2
+  parseVersionOutput :: String -> _ -> Spago (LogEnv a) Purs
+  parseVersionOutput cmd result = case parseLenientVersion (dropStuff "-" $ dropStuff " " result.stdout) of
+    Left _err -> die $ "Failed to parse purs version. Was: " <> result.stdout
+    -- Fail if Purs is lower than 0.15.4
+    Right v ->
+      if Version.minor v >= 15 && Version.patch v >= 4 then
+        pure { cmd, version: v }
+      else
+        die [ "Unsupported PureScript version " <> Version.print v, "Please install PureScript v0.15.4 or higher." ]
 
 compile :: forall a. Set FilePath -> Array String -> Spago (PursEnv a) (Either Cmd.ExecError Cmd.ExecResult)
 compile globs pursArgs = do
