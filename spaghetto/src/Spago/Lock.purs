408552091ae5b917a183d50b5bd86ed89d0470d0
Thomas Honeyman <hello@thomashoneyman.com>
2023-05-23 15:12:23 -0500

    Update lockfile format to yaml, format files

--- spaghetto/src/Spago/Lock.purs
+++ spaghetto/src/Spago/Lock.purs
@@ -1,123 +1,109 @@
-module Spago.Lock where
+module Spago.Lock
+  ( Lockfile
+  , lockfileCodec
+  , LockEntry(..)
+  , lockEntryCodec
+  , PathLock
+  , GitLock
+  , RegistryLock
+  ) where
 
 import Spago.Prelude
 
-import Data.Argonaut.Core (Json)
 import Data.Codec.Argonaut as CA
 import Data.Codec.Argonaut.Record as CA.Record
-import Data.String as String
-import Parsing (Parser)
-import Parsing as Parsing
-import Parsing.Combinators as Parsing.Combinators
-import Parsing.String as Parsing.String
-import Registry.Internal.Codec as Registry.Internal
+import Data.Profunctor as Profunctor
+import Record as Record
+import Registry.Internal.Codec as Registry.Codec
 import Registry.Sha256 as Sha256
 import Registry.Version as Version
+import Type.Proxy (Proxy(..))
 
 type Lockfile =
-  { lockfile :: Version
-  , packages :: Map PackageName LockEntry
+  { packages :: Map PackageName LockEntry
   }
 
 lockfileCodec :: JsonCodec Lockfile
 lockfileCodec = CA.Record.object "Lockfile"
-  { lockfile: Version.codec
-  , packages: Registry.Internal.packageMap lockEntryCodec
+  { packages: Registry.Codec.packageMap lockEntryCodec
   }
 
 data LockEntry
-  -- | A package in the local workspace, which we won't hash; workspace packages
-  -- | are essentially mutable.
-  = WorkspaceLock { path :: FilePath }
-  -- | A Git checkout of a package, in which we know the commit SHA-1.
-  | GitLock { url :: String, rev :: String, subdir :: Maybe FilePath }
-  -- | A registry package at a specific version, in which we can read the hash
-  -- | from the registry metadata.
-  | RegistryLock { version :: Version, hash :: Sha256 }
+  = FromPath PathLock
+  | FromGit GitLock
+  | FromRegistry RegistryLock
 
 derive instance Eq LockEntry
 
 lockEntryCodec :: JsonCodec LockEntry
 lockEntryCodec = CA.codec' decode encode
   where
-  encode :: LockEntry -> Json
-  encode = CA.encode CA.string <<< printLockEntry
+  encode = case _ of
+    FromPath lock -> CA.encode pathLockCodec lock
+    FromGit lock -> CA.encode gitLockCodec lock
+    FromRegistry lock -> CA.encode registryLockCodec lock
 
-  decode :: Json -> Either JsonDecodeError LockEntry
-  decode json = do
-    string <- CA.decode CA.string json
-    lmap CA.TypeMismatch (parseLockEntry string)
+  decode json =
+    map FromPath (CA.decode pathLockCodec json)
+      <|> map FromGit (CA.decode gitLockCodec json)
+      <|> map FromRegistry (CA.decode registryLockCodec json)
 
-printLockEntry :: LockEntry -> String
-printLockEntry = case _ of
-  WorkspaceLock { path } ->
-    workspacePrefix <> path
-  GitLock { url, rev, subdir } -> do
-    let hashlock = gitPrefix <> url <> "#" <> rev
-    maybe hashlock (\path -> hashlock <> "?subdir=" <> path) subdir
-  RegistryLock { version, hash } ->
-    registryPrefix <> Version.print version <> "#" <> Sha256.print hash
+pathLockType :: String
+pathLockType = "local"
 
-parseLockEntry :: String -> Either String LockEntry
-parseLockEntry = lmap Parsing.parseErrorMessage <<< flip Parsing.runParser lockEntryParser
+gitLockType :: String
+gitLockType = "git"
 
-lockEntryParser :: Parser String LockEntry
-lockEntryParser = do
-  let prefixError = "Expected an entry prefixed with one of: " <> String.joinWith ", " prefixes
+registryLockType :: String
+registryLockType = "registry"
 
-  prefix <-
-    Parsing.Combinators.choice
-      [ Parsing.String.string workspacePrefix
-      , Parsing.String.string gitPrefix
-      , Parsing.String.string registryPrefix
-      , Parsing.fail prefixError
-      ]
+type PathLock = { path :: FilePath }
 
-  if prefix == workspacePrefix then do
-    path <- Parsing.String.rest
-    pure $ WorkspaceLock { path }
+pathLockCodec :: JsonCodec PathLock
+pathLockCodec = Profunctor.dimap toRep fromRep $ CA.object "PathLock"
+  $ CA.recordProp (Proxy :: _ "type") (constant pathLockType)
+  $ CA.recordProp (Proxy :: _ "path") CA.string
+  $ CA.record
+  where
+  toRep = Record.insert (Proxy :: _ "type") pathLockType
+  fromRep = Record.delete (Proxy :: _ "type")
 
-  else if prefix == gitPrefix then do
-    Tuple url _ <- Parsing.String.anyTill (Parsing.String.char '#') <|> Parsing.fail "Expected a URL terminated by a #"
-    -- Git SHA-1 commit hashes are 40 characters long.
-    rev <- Parsing.String.takeN 40 <|> Parsing.fail "Expected a 40-character commit hash"
-    -- The subdir key is optional
-    subdir <- Parsing.Combinators.choice
-      [ do
-          _ <- Parsing.String.eof
-          pure Nothing
-      , do
-          _ <- Parsing.String.string "?subdir="
-          dir <- Parsing.String.rest
-          pure $ Just dir
-      , do
-          extra <- Parsing.String.rest
-          Parsing.fail $ "Expected end of string or a ?subdir= entry but received: " <> extra
-      ]
-    pure $ GitLock { url, rev, subdir }
+type GitLock =
+  { url :: String
+  , rev :: String
+  , subdir :: Maybe FilePath
+  }
 
-  else if prefix == registryPrefix then do
-    Tuple rawVersion _ <- Parsing.String.anyTill (Parsing.String.char '#')
-    version <- case Version.parse rawVersion of
-      Left error -> Parsing.fail error
-      Right version -> pure version
-    rawHash <- Parsing.String.rest
-    hash <- case Sha256.parse rawHash of
-      Left error -> Parsing.fail error
-      Right hash -> pure hash
-    pure $ RegistryLock { version, hash }
+gitLockCodec :: JsonCodec GitLock
+gitLockCodec = Profunctor.dimap toRep fromRep $ CA.object "GitLock"
+  $ CA.recordProp (Proxy :: _ "type") (constant gitLockType)
+  $ CA.recordProp (Proxy :: _ "url") CA.string
+  $ CA.recordProp (Proxy :: _ "rev") CA.string
+  $ CA.recordPropOptional (Proxy :: _ "subdir") CA.string
+  $ CA.record
+  where
+  toRep = Record.insert (Proxy :: _ "type") gitLockType
+  fromRep = Record.delete (Proxy :: _ "type")
 
-  else
-    Parsing.fail prefixError
+type RegistryLock =
+  { version :: Version
+  , integrity :: Sha256
+  }
 
-prefixes :: Array String
-prefixes = [ workspacePrefix, gitPrefix, registryPrefix ]
+registryLockCodec :: JsonCodec RegistryLock
+registryLockCodec = Profunctor.dimap toRep fromRep $ CA.object "RegistryLock"
+  $ CA.recordProp (Proxy :: _ "type") (constant registryLockType)
+  $ CA.recordProp (Proxy :: _ "version") Version.codec
+  $ CA.recordProp (Proxy :: _ "integrity") Sha256.codec
+  $ CA.record
+  where
+  toRep = Record.insert (Proxy :: _ "type") registryLockType
+  fromRep = Record.delete (Proxy :: _ "type")
 
-workspacePrefix :: String
-workspacePrefix = "workspace:"
-
-gitPrefix :: String
-gitPrefix = "git:"
-
-registryPrefix :: String
-registryPrefix = "registry:"
+constant :: String → JsonCodec String
+constant val = CA.codec' decode encode
+  where
+  encode = CA.encode CA.string
+  decode json = CA.decode CA.string json >>= case _ of
+    decoded | decoded == val -> pure val
+    _ -> Left (CA.UnexpectedValue json)
--- spaghetto/test/Spago/Lock.purs
+++ spaghetto/test/Spago/Lock.purs
@@ -2,9 +2,12 @@ module Test.Spago.Lock where
 
 import Spago.Prelude
 
-import Data.Argonaut.Core (Json)
-import Data.Argonaut.Parser as Argonaut
 import Data.Codec.Argonaut as CA
+import Data.Map as Map
+import Registry.PackageName as PackageName
+import Registry.Sha256 as Sha256
+import Registry.Version as Version
+import Spago.Lock (LockEntry(..), Lockfile)
 import Spago.Lock as Lock
 import Test.Spec (Spec)
 import Test.Spec as Spec
@@ -12,50 +15,59 @@ import Test.Spec.Assertions as Assert
 
 spec :: Spec Unit
 spec = do
-  Spec.describe "Parses lockfile formats" do
-    Spec.it "workspace:" do
-      case Lock.parseLockEntry "workspace:./my-library" of
-        Left error -> Assert.fail error
-        Right (Lock.WorkspaceLock { path }) | path == "./my-library" -> pure unit
-        _ -> Assert.fail "Expected 'WorkspaceLock' with path './my-library'"
+  Spec.it "Parses lockfile" do
+    case parseYaml Lock.lockfileCodec validLockfileString of
+      Left error ->
+        Assert.fail $ "Failed to parse: " <> CA.printJsonDecodeError error
+      Right lock | lock /= validLockfile ->
+        Assert.fail ("\n" <> printYaml Lock.lockfileCodec lock <> "\ndoes not equal\n\n" <> printYaml Lock.lockfileCodec validLockfile)
+      Right _ ->
+        pure unit
 
-    Spec.it "git:" do
-      case Lock.parseLockEntry "git:https://github.com/purescript/purescript-console.git#3b83d7b792d03872afeea5e62b4f686ab0f09842" of
-        Left error -> Assert.fail error
-        Right (Lock.GitLock { rev, url, subdir })
-          | rev == "3b83d7b792d03872afeea5e62b4f686ab0f09842"
-          , url == "https://github.com/purescript/purescript-console.git"
-          , subdir == Nothing -> pure unit
-        _ -> Assert.fail "Expected 'GitLock'"
-
-      case Lock.parseLockEntry "git:https://github.com/purescript/purescript-console.git#3b83d7b792d03872afeea5e62b4f686ab0f09842?subdir=console" of
-        Left error -> Assert.fail error
-        Right (Lock.GitLock { rev, url, subdir })
-          | rev == "3b83d7b792d03872afeea5e62b4f686ab0f09842"
-          , url == "https://github.com/purescript/purescript-console.git"
-          , subdir == Just "console" -> pure unit
-        _ -> Assert.fail "Expected 'GitLock'"
-
-    Spec.it "registry:" do
-      case Lock.parseLockEntry "registry:4.0.0#sha256-eBtZu+HZcMa5HilvI6kaDyVX3ji8p0W9MGKy2K4T6+M=" of
-        Left error -> Assert.fail error
-        Right _ -> pure unit
-
-    Spec.it "lockfile" do
-      case CA.decode Lock.lockfileCodec validLockfile of
-        Left error -> Assert.fail (CA.printJsonDecodeError error)
-        Right _ -> pure unit
-
-validLockfile :: Json
-validLockfile = unsafeFromRight $ Argonaut.jsonParser
-  """
-  {
-    "lockfile": "1.0.0",
-    "packages": {
-      "console": "git:https://github.com/purescript/purescript-console.git#3b83d7b792d03872afeea5e62b4f686ab0f09842",
-      "effect": "registry:4.0.0#sha256-eBtZu+HZcMa5HilvI6kaDyVX3ji8p0W9MGKy2K4T6+M=",
-      "my-library": "workspace:./my-library",
-      "prelude": "git:https://github.com/purescript/purescript-libraries.git#3b83d7b792d03872afeea5e62b4f686ab0f09842?subdir=prelude"
-    }
+validLockfile :: Lockfile
+validLockfile =
+  { packages:
+      Map.fromFoldable
+        [ packageTuple "console" $ FromGit
+            { url: "https://github.com/purescript/purescript-console.git"
+            , rev: "3b83d7b792d03872afeea5e62b4f686ab0f09842"
+            , subdir: Nothing
+            }
+        , packageTuple "effect" $ FromRegistry
+            { version: unsafeFromRight (Version.parse "4.0.0")
+            , integrity: unsafeFromRight (Sha256.parse "sha256-eBtZu+HZcMa5HilvI6kaDyVX3ji8p0W9MGKy2K4T6+M=")
+            }
+        , packageTuple "my-library" $ FromPath
+            { path: "my-library"
+            }
+        , packageTuple "prelude" $ FromGit
+            { url: "https://github.com/purescript/purescript-libraries.git"
+            , rev: "3b83d7b792d03872afeea5e62b4f686ab0f09842"
+            , subdir: Just "prelude"
+            }
+        ]
   }
+  where
+  packageTuple name = Tuple (unsafeFromRight (PackageName.parse name))
+
+validLockfileString :: String
+validLockfileString =
+  """
+  packages:
+    console:
+      type: git
+      url: https://github.com/purescript/purescript-console.git
+      rev: 3b83d7b792d03872afeea5e62b4f686ab0f09842
+    effect:
+      type: registry
+      version: 4.0.0
+      integrity: sha256-eBtZu+HZcMa5HilvI6kaDyVX3ji8p0W9MGKy2K4T6+M=
+    my-library:
+      type: local
+      path: my-library
+    prelude:
+      type: git
+      url: https://github.com/purescript/purescript-libraries.git
+      rev: 3b83d7b792d03872afeea5e62b4f686ab0f09842
+      subdir: prelude
   """
