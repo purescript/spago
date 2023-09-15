150e50596f2174c1c605462acef40f36645be296
Fabrizio Ferrai <fabrizio.ferrai@gmail.com>
2023-09-15 17:03:19 +0300

    Replace Haskell CI with PureScript CI

--- src/Spago/Esbuild.purs
+++ src/Spago/Esbuild.purs
@@ -6,13 +6,13 @@ import Spago.Cmd as Cmd
 
 type Esbuild =
   { cmd :: FilePath
-  , version :: String 
+  , version :: String
   }
 
 getEsbuild :: forall a. Spago (LogEnv a) Esbuild
-getEsbuild = 
-  Cmd.exec "esbuild" ["--version"] Cmd.defaultExecOptions { pipeStdout = false, pipeStderr = false } >>= case _ of
+getEsbuild =
+  Cmd.exec "esbuild" [ "--version" ] Cmd.defaultExecOptions { pipeStdout = false, pipeStderr = false } >>= case _ of
     Left err -> do
       logDebug $ show err
-      die [ "Failed to find esbuild. See https://esbuild.github.io/getting-started/#install-esbuild for ways to install esbuild." ] 
+      die [ "Failed to find esbuild. See https://esbuild.github.io/getting-started/#install-esbuild for ways to install esbuild." ]
     Right r -> pure { cmd: "esbuild", version: r.stdout }
