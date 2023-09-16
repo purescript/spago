0f0a804b8e3fe312cea5021155bb569e45888756
Fabrizio Ferrai <fabrizio.ferrai@gmail.com>
2023-09-16 12:11:39 +0300

    Bootstrap executable

--- src/Spago/Esbuild.purs
+++ src/Spago/Esbuild.purs
@@ -13,6 +13,6 @@ getEsbuild :: forall a. Spago (LogEnv a) Esbuild
 getEsbuild =
   Cmd.exec "esbuild" [ "--version" ] Cmd.defaultExecOptions { pipeStdout = false, pipeStderr = false } >>= case _ of
     Left err -> do
-      logDebug $ show err
+      logInfo $ show err
       die [ "Failed to find esbuild. See https://esbuild.github.io/getting-started/#install-esbuild for ways to install esbuild." ]
     Right r -> pure { cmd: "esbuild", version: r.stdout }
