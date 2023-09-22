7f173033aafffd0303021e2bbab3f012d062d893
JordanMartinez <jordanalex.martinez@protonmail.com>
2023-09-22 02:30:28 -0500

    Add findFlags test; fix implementation (#1010)

---
+++ test/Spago/FindFlags.purs
@@ -0,0 +1,32 @@
+module Test.Spago.FindFlags where
+
+import Prelude
+
+import Data.Maybe (fromMaybe)
+import Spago.Cmd as Cmd
+import Test.Spec (Spec, describe, describeOnly, it)
+import Test.Spec.Assertions (shouldEqual)
+
+spec :: Spec Unit
+spec = do
+  describeOnly "findFlag" $ do
+    it "[\"-o\", \"something\"]" $ do
+      let a = fromMaybe "" $ Cmd.findFlag { flags: [ "-o", "--output" ], args: [ "-o", "something" ] }
+      let b = "something"
+      a `shouldEqual` b
+    it "[\"--output\", \"something\"]" $ do
+      let a = fromMaybe "" $ Cmd.findFlag { flags: [ "-o", "--output" ], args: [ "--output", "something" ] }
+      let b = "something"
+      a `shouldEqual` b
+    it "[\"-o something\"]" $ do
+      let a = fromMaybe "" $ Cmd.findFlag { flags: [ "-o", "--output" ], args: [ "-o something" ] }
+      let b = "something"
+      a `shouldEqual` b
+    it "[\"--output something\"]" $ do
+      let a = fromMaybe "" $ Cmd.findFlag { flags: [ "-o", "--output" ], args: [ "--output something" ] }
+      let b = "something"
+      a `shouldEqual` b
+    it "[\"--output=something\"]" $ do
+      let a = fromMaybe "" $ Cmd.findFlag { flags: [ "-o", "--output" ], args: [ "--output=something" ] }
+      let b = "something"
+      a `shouldEqual` b
