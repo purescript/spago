6f7ce28eb57e8049f7de7f3494e6684e014b8398
Jordan Martinez <jordanalex.martinez@protonmail.com>
2023-09-29 08:48:17 -0500

    Get rid of misc compiler warnings

--- test/Spago/Docs.purs
+++ test/Spago/Docs.purs
@@ -2,15 +2,8 @@ module Test.Spago.Docs where
 
 import Test.Prelude
 
-import Node.FS.Aff as FSA
-import Node.Path as Path
-import Registry.Version as Version
-import Spago.Command.Init as Init
-import Spago.Core.Config as Config
-import Spago.FS as FS
 import Test.Spec (Spec)
 import Test.Spec as Spec
-import Test.Spec.Assertions as Assert
 
 spec :: Spec Unit
 spec = Spec.around withTempDir do
