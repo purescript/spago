1935d2a909b32bac2d886dbfb7c34bf062c938d7
Charles Taylor <charlestaylor95@gmail.com>
2023-10-04 08:54:40 -0400

    Unformat file

--- docs-search/client-halogen/src/Docs/Search/BrowserEngine.js
+++ docs-search/client-halogen/src/Docs/Search/BrowserEngine.js
@@ -1,23 +1,24 @@
 /* global exports */
 
-export function loadIndex_(partId) {
+export function loadIndex_ (partId) {
   return function (url) {
     return function () {
-      return new Promise(function (resolve, reject) {
-        if (typeof window.DocsSearchIndex[partId] === "undefined") {
-          var script = document.createElement("script");
-          script.type = "text/javascript";
+      return new Promise(function(resolve, reject) {
+        if (typeof window.DocsSearchIndex[partId] === 'undefined') {
+
+          var script = document.createElement('script');
+          script.type = 'text/javascript';
           script.src = url;
 
-          script.addEventListener("load", function () {
-            if (typeof window.DocsSearchIndex[partId] == "undefined") {
+          script.addEventListener('load', function () {
+            if (typeof window.DocsSearchIndex[partId] == 'undefined') {
               reject();
             } else {
               resolve(window.DocsSearchIndex[partId]);
             }
           });
 
-          script.addEventListener("error", reject);
+          script.addEventListener('error', reject);
 
           document.body.appendChild(script);
         } else {
@@ -26,4 +27,4 @@ export function loadIndex_(partId) {
       });
     };
   };
-}
+};
