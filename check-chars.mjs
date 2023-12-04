2c643053c0bf19ab586367c22cd2c73553f6f3a6
Jordan Martinez <jordanalex.martinez@protonmail.com>
2023-12-04 14:35:41 -0600

    Sanity check parameters

--- check-chars.mjs
+++ check-chars.mjs
@@ -1,14 +1,15 @@
 import cp from "node:child_process";
 
 const echo = "echo";
-const space = " ";
-const maxCharLimit = 8191 - echo.length - space.length - 1
+const maxCharLimit = 8191
 let initialArgs = "";
+let initialParams = [];
 for (let index = 0; index < maxCharLimit; index++) {
   initialArgs = initialArgs + "0";  
+  initialParams.push("0");
 }
 
-const go = (args) => {
+const checkCharCount = (args) => {
   console.log(`Attempting to spawn 'echo X' where 'X' is ${args.length} chars`);
   const result = cp.spawn(echo, [ args ], { stdio: [null, null, null]});
   result.on("close", (code, sig) => {
@@ -24,4 +25,24 @@ const go = (args) => {
   });
 };
 
-go(initialArgs);
+checkCharCount(initialArgs);
+
+
+const checkParamCount = (params) => {
+  console.log(`Attempting to spawn 'echo X...' where 'X' is ${params.length} parameters`);
+  const result = cp.spawn(echo, params, { stdio: [null, null, null]});
+  result.on("close", (code, sig) => {
+    if (code === 1 || sig) {
+      go(params.slice(0, -1));
+    } else if (code === 0) {
+      console.log(`Successfully spawned with ${params.length} parameters`);
+    } else {
+      console.log("Something else happened.");
+      console.log(exit);
+      console.log(sig);
+    }
+  });
+};
+
+checkParamCount(initialParams);
+
