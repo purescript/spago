e2bafaa3f76922c2b1fd07be8dbe5242544a6610
Jordan Martinez <jordanalex.martinez@protonmail.com>
2024-01-10 16:54:05 -0600

    Increase docs tests' timeout to 5s

--- docs-search/index/test/Docs/Search/Main.purs
+++ docs-search/index/test/Docs/Search/Main.purs
@@ -2,19 +2,29 @@ module Test.Docs.Search.Main where
 
 import Prelude
 
+import Data.Identity (Identity(..))
+import Data.Maybe (Maybe(..))
+import Data.Newtype (un)
 import Effect (Effect)
-import Effect.Aff (launchAff_)
+import Effect.Aff (Milliseconds(..), launchAff_)
 import Test.Declarations as Declarations
 import Test.IndexBuilder as IndexBuilder
 import Test.ModuleIndex as ModuleIndex
 import Test.ModuleParser as ModuleParser
-import Test.TypeQuery as TypeQuery
 import Test.Spec (Spec)
 import Test.Spec.Reporter.Console (consoleReporter)
-import Test.Spec.Runner (runSpec)
+import Test.Spec.Runner (Config, defaultConfig, runSpecT)
+import Test.TypeQuery as TypeQuery
+
+
+testConfig :: Config
+testConfig = defaultConfig
+  { slow = Milliseconds 2_000.0
+  , timeout = Just (Milliseconds 5_000.0)
+  }
 
 main :: Effect Unit
-main = launchAff_ $ runSpec [ consoleReporter ] mainTest
+main = launchAff_ $ void $ un Identity $ runSpecT testConfig [ consoleReporter ] mainTest
 
 mainTest :: Spec Unit
 mainTest = do
