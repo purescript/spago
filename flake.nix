67b457ca79e5668a6a35649b88cdaa94b97cf81d
Danila Danko <br4ch1st0chr0n3@gmail.com>
2024-01-29 01:41:38 +0300

    chore: update nixpkgs

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     purescript-overlay.url = "github:thomashoneyman/purescript-overlay";
     purescript-overlay.inputs.nixpkgs.follows = "nixpkgs";
   };
