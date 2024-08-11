6ecc71610eaa37e2a2fe980db6118b78bb63bb71
Fyodor Soikin <name.fa@gmail.com>
2024-08-10 22:22:27 -0400

    Fix tests

--- test/Spago/Graph.purs
+++ test/Spago/Graph.purs
@@ -10,25 +10,25 @@ spec = Spec.around withTempDir do
   Spec.describe "graph" do
 
     Spec.it "can output the module graph in JSON" \{ spago, fixture } -> do
-      spago [ "init", "--name", "my-project", "--package-set", "41.2.0" ] >>= shouldBeSuccess
+      spago [ "init", "--name", "my-project", "--package-set", "56.4.0" ] >>= shouldBeSuccess
       spago [ "graph", "modules", "--json" ] >>= shouldBeSuccessOutput (fixture "graph-modules.json")
 
     Spec.it "can output the module graph for graphviz" \{ spago, fixture } -> do
-      spago [ "init", "--name", "my-project", "--package-set", "41.2.0" ] >>= shouldBeSuccess
+      spago [ "init", "--name", "my-project", "--package-set", "56.4.0" ] >>= shouldBeSuccess
       spago [ "graph", "modules", "--dot" ] >>= shouldBeSuccessOutput (fixture "graph-modules.dot")
 
     Spec.it "can topologically sort modules" \{ spago, fixture } -> do
-      spago [ "init", "--name", "my-project", "--package-set", "41.2.0" ] >>= shouldBeSuccess
+      spago [ "init", "--name", "my-project", "--package-set", "56.4.0" ] >>= shouldBeSuccess
       spago [ "graph", "modules", "--topo" ] >>= shouldBeSuccessOutput (fixture "graph-modules-topo.txt")
 
     Spec.it "can output the package graph in JSON" \{ spago, fixture } -> do
-      spago [ "init", "--name", "my-project", "--package-set", "41.2.0" ] >>= shouldBeSuccess
+      spago [ "init", "--name", "my-project", "--package-set", "56.4.0" ] >>= shouldBeSuccess
       spago [ "graph", "packages", "--json" ] >>= shouldBeSuccessOutput (fixture "graph-packages.json")
 
     Spec.it "can output the package graph for graphviz" \{ spago, fixture } -> do
-      spago [ "init", "--name", "my-project", "--package-set", "41.2.0" ] >>= shouldBeSuccess
+      spago [ "init", "--name", "my-project", "--package-set", "56.4.0" ] >>= shouldBeSuccess
       spago [ "graph", "packages", "--dot" ] >>= shouldBeSuccessOutput (fixture "graph-packages.dot")
 
     Spec.it "can topologically sort packages" \{ spago, fixture } -> do
-      spago [ "init", "--name", "my-project", "--package-set", "41.2.0" ] >>= shouldBeSuccess
+      spago [ "init", "--name", "my-project", "--package-set", "56.4.0" ] >>= shouldBeSuccess
       spago [ "graph", "packages", "--topo" ] >>= shouldBeSuccessOutput (fixture "graph-packages-topo.txt")
