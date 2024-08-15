e528a964dce28c81589d82ee60d47b8631798d18
Serhii Khoma <srghma@gmail.com>
2024-08-15 16:33:47 +0700

    fix: error "Could not parse any remotes from the output of `git remote --verbose`."
    $ ~/projects/spago/bin/index.dev.js publish -p open-drawing --verbose
    ....
    [    9085ms] Got error while reading metadata file: ENOENT: no such file or directory, open '/home/srghma/.cache/spago-nodejs/registry/metadata/open-drawing.json'
    asdf: [
      'origin',
      'ssh://git@github.com/purescript-open-community/purescript-open-drawing.git',
      '(fetch)'
    ]
    asdf: Nothing {}
    asdf: [
      'origin',
      'ssh://git@github.com/purescript-open-community/purescript-open-drawing.git',
      '(push)'
    ]
    asdf: Nothing {}
    ✘ Could not parse any remotes from the output of `git remote --verbose`.
    
    $ git remote --verbose
    origin  ssh://git@github.com/purescript-open-community/purescript-open-drawing.git (fetch)
    origin  ssh://git@github.com/purescript-open-community/purescript-open-drawing.git (push)

--- test/Spago/Unit/Git.purs
+++ test/Spago/Unit/Git.purs
@@ -15,14 +15,30 @@ spec = do
       Spec.it "parses a remote with a git protocol" do
         parseRemote "origin\tgit@github.com:foo/bar.git (fetch)"
           `shouldEqual` Just { name: "origin", url: "git@github.com:foo/bar.git", owner: "foo", repo: "bar" }
+        parseRemote "origin  git@github.com:foo/bar.git (fetch)"
+          `shouldEqual` Just { name: "origin", url: "git@github.com:foo/bar.git", owner: "foo", repo: "bar" }
 
       Spec.it "parses a remote with an https protocol" do
         parseRemote "origin\thttps://github.com/foo/bar.git (push)"
           `shouldEqual` Just { name: "origin", url: "https://github.com/foo/bar.git", owner: "foo", repo: "bar" }
+        parseRemote "origin  https://github.com/foo/bar.git (push)"
+          `shouldEqual` Just { name: "origin", url: "https://github.com/foo/bar.git", owner: "foo", repo: "bar" }
+
+      Spec.it "parses a remote with an ssh protocol" do
+        parseRemote "origin\tssh://git@github.com/foo/bar.git (push)"
+          `shouldEqual` Just { name: "origin", url: "ssh://git@github.com/foo/bar.git", owner: "foo", repo: "bar" }
+        parseRemote "origin  ssh://git@github.com/foo/bar.git (push)"
+          `shouldEqual` Just { name: "origin", url: "ssh://git@github.com/foo/bar.git", owner: "foo", repo: "bar" }
 
       Spec.it "rejects malformed remotes" do
         parseRemote "origin\tgit@github.com:foo/bar.git" `shouldEqual` Nothing
+        parseRemote "origin  git@github.com:foo/bar.git" `shouldEqual` Nothing
+
         parseRemote "origin\tgit@github.com:foo/bar (push)" `shouldEqual` Nothing
-        parseRemote "origin git@github.com:foo/bar.git (fetch)" `shouldEqual` Nothing
+        parseRemote "origin  git@github.com:foo/bar (push)" `shouldEqual` Nothing
+
         parseRemote "origin\tgit@github.com:foo.git (push)" `shouldEqual` Nothing
+        parseRemote "origin  git@github.com:foo.git (push)" `shouldEqual` Nothing
+
         parseRemote "origin\thttps://foo.com/bar.git (push)" `shouldEqual` Nothing
+        parseRemote "origin  https://foo.com/bar.git (push)" `shouldEqual` Nothing
