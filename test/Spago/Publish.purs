8087c2297770fe6600feae7d288d9ddeb5770ec0
Fyodor Soikin <name.fa@gmail.com>
2024-09-06 11:32:11 -0400

    In spago publish, when registry solver returns package versions not in local cache, install them before trying to build

--- test/Spago/Publish.purs
+++ test/Spago/Publish.purs
@@ -2,6 +2,9 @@ module Test.Spago.Publish (spec) where
 
 import Test.Prelude
 
+import Data.String as String
+import Data.String.Regex as Regex
+import Data.String.Regex.Flags as RF
 import Node.Platform as Platform
 import Node.Process as Process
 import Spago.Cmd as Cmd
@@ -47,7 +50,7 @@ spec = Spec.around withTempDir do
       FS.copyFile { src: fixture "publish.purs", dst: "src/Main.purs" }
       spago [ "build" ] >>= shouldBeSuccess
       doTheGitThing
-      git [ "remote", "set-url", "origin", "git@github.com:purescript/bbb.git" ] >>= shouldBeSuccess
+      git [ "remote", "set-url", "origin", "git@github.com:purescript/bbb.git" ]
       spago [ "publish", "--offline" ] >>= shouldBeFailureErr (fixture "publish-invalid-location.txt")
 
     Spec.it "fails if a core dependency is not in the registry" \{ spago, fixture } -> do
@@ -74,18 +77,64 @@ spec = Spec.around withTempDir do
       spago [ "fetch" ] >>= shouldBeSuccess
       spago [ "publish", "--offline" ] >>= shouldBeFailureErr (fixture "publish.txt")
 
+    Spec.it "#1110 installs versions of packages that are returned by the registry solver, but not present in cache" \{ spago, fixture } -> do
+      let
+        shouldBeFailureErr' file = checkOutputs'
+          { stdoutFile: Nothing
+          , stderrFile: Just file
+          , result: isLeft
+          , sanitize: String.trim >>> Regex.replace buildOrderRegex "[x of 3] Compiline module-name"
+          }
+
+        -- We have to ignore lines like "[1 of 3] Compiling Effect.Console" when
+        -- comparing output, because the compiler will always compile in
+        -- different order, depending on how the system resources happened to
+        -- align at the moment of the test run.
+        buildOrderRegex = unsafeFromRight $ Regex.regex
+          "\\[\\d of 3\\] Compiling (Effect\\.Console|Effect\\.Class\\.Console|Lib)\n" RF.global
+
+      FS.copyTree { src: fixture "publish/1110-solver-different-version", dst: "." }
+      spago [ "build" ] >>= shouldBeSuccess
+      doTheGitThing
+      spago [ "fetch" ] >>= shouldBeSuccess
+
+      -- The local `spago.yaml` specifies `console: 6.0.0` in `extraPackages`,
+      -- so that's what should be in local cache after running `fetch`.
+      -- Importantly, `console-6.1.0` should not be there yet.
+      FS.exists ".spago/p/console-6.0.0" >>= (_ `shouldEqual` true)
+      FS.exists ".spago/p/console-6.1.0" >>= (_ `shouldEqual` false)
+
+      spago [ "publish", "--offline" ] >>= shouldBeFailureErr' (fixture "publish/1110-solver-different-version/expected-stderr.txt")
+
+      -- When `publish` runs, it uses the registry solver, which returns
+      -- `console-6.1.0` version, so `publish` should fetch that into local
+      -- cache and build with it.
+      FS.exists ".spago/p/console-6.1.0" >>= (_ `shouldEqual` true)
+
+      -- Now screw up the `console-6.1.0` package in the local cache, so that it
+      -- doesn't compile anymore, and check that the relevant compile error
+      -- happens on publish.
+      FS.unlink ".spago/p/console-6.1.0/src/Effect/Console.js"
+      rmRf ".spago/p/console-6.1.0/output"
+      spago [ "publish", "--offline" ] >>= shouldBeFailureErr' (fixture "publish/1110-solver-different-version/failure-stderr.txt")
+
 doTheGitThing :: Aff Unit
 doTheGitThing = do
-  git [ "init" ] >>= shouldBeSuccess
-  git [ "config", "user.name", "test-user" ] >>= shouldBeSuccess
-  git [ "config", "user.email", "test-user@aol.com" ] >>= shouldBeSuccess
-  git [ "config", "commit.gpgSign", "false" ] >>= shouldBeSuccess
-  git [ "config", "tag.gpgSign", "false" ] >>= shouldBeSuccess
-  git [ "add", "." ] >>= shouldBeSuccess
-  git [ "commit", "-m", "first" ] >>= shouldBeSuccess
-  git [ "tag", "v0.0.1" ] >>= shouldBeSuccess
-  git [ "remote", "add", "origin", "git@github.com:purescript/aaa.git" ] >>= shouldBeSuccess
+  git [ "init" ]
+  git [ "config", "user.name", "test-user" ]
+  git [ "config", "user.email", "test-user@aol.com" ]
+  git [ "config", "commit.gpgSign", "false" ]
+  git [ "config", "tag.gpgSign", "false" ]
+  git [ "add", "." ]
+  git [ "commit", "-m", "first" ]
+  git [ "tag", "v0.0.1" ]
+  git [ "remote", "add", "origin", "git@github.com:purescript/aaa.git" ]
 
-git :: Array String -> Aff (Either ExecResult ExecResult)
-git args = Cmd.exec "git" args
-  $ Cmd.defaultExecOptions { pipeStdout = false, pipeStderr = false, pipeStdin = StdinNewPipe }
+git :: Array String -> Aff Unit
+git = git' Nothing
+
+git' :: Maybe FilePath -> Array String -> Aff Unit
+git' cwd args =
+  Cmd.exec "git" args
+    (Cmd.defaultExecOptions { pipeStdout = false, pipeStderr = false, pipeStdin = StdinNewPipe, cwd = cwd })
+    >>= shouldBeSuccess
