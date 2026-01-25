4e28ff03d0bbbf7324973438a360b9f047000961
Fabrizio Ferrai <fabrizio.ferrai@gmail.com>
2026-01-25 02:30:05 +0200

    Fix tests

--- bin/src/Main.purs
+++ bin/src/Main.purs
@@ -1012,11 +1012,7 @@ mkRegistryEnv offline = do
   pure
     { getRegistry: Registry.getRegistryFns registryBox registryLock
     , logOptions
-    -- we only need OnlineRefreshRegistry when actually pulling the registry.
-    -- Once that's done we can reset to a more normal value
-    , offline: case offline of
-        OnlineRefreshRegistry -> Online
-        other -> other
+    , offline
     , purs
     , git
     , db
